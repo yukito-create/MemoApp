0917f1c99f9c4ef83062e4a4e74bf6a78480da1c
Yukito <193806322+yukito-create@users.noreply.github.com>
2025-12-05 14:05:43 +0900

    redirect implementation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,7 @@
-import { View, StyleSheet } from "react-native"
+import { Redirect } from "expo-router"
 
-import Header from "../components/Header"
-import MemoListItem from "../components/MemoListItem"
-import CircleButton from "../components/CircleButton"
-
-const Index = (): React.JSX.Element  => {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
-      <CircleButton>+</CircleButton>
-    </View>
-  )
+const Index = (): React.JSX.Element => {
+  return <Redirect href="memo/list" />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff"
-  }
-})
 
 export default Index
