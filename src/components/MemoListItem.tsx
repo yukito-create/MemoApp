9d5056e4af941a8d1df536c25247e0c909f535b6
Yukito <193806322+yukito-create@users.noreply.github.com>
2025-12-06 18:17:18 +0900

    provisional completed. need to fix navigation of listPage.

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,11 +1,32 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native"
 import { Link } from "expo-router"
+import { deleteDoc, doc } from "firebase/firestore"
 
 import Icon from "./Icon"
 import { type Memo } from "../../types/memo"
+import { auth, db } from "../config"
 
 interface Props {
   memo: Memo
+}
+
+const handlePress = (id: string): void => {
+  if (auth.currentUser === null) { return }
+
+  const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+  Alert.alert("メモを削除します", "よろしいですか？", [
+    {
+      text: "キャンセル"
+    },
+    {
+      text: "削除する",
+      style: "destructive",
+      onPress: () => {
+        deleteDoc(ref)
+          .catch((error) => { Alert.alert("削除に失敗しました")})
+      }
+    }
+  ])
 }
 
 const MemoListItem = (props: Props): React.JSX.Element | null => {
@@ -24,7 +45,7 @@ const MemoListItem = (props: Props): React.JSX.Element | null => {
           <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
           <Text style={styles.memoListItemDate}>{dateString}</Text>
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => { handlePress(memo.id) }}>
           <Icon name="delete" size={32} color="#B0B0B0" />
         </TouchableOpacity>
       </TouchableOpacity>
