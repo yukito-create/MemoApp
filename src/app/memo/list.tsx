3f33a7f5b2d981412bc026db549433d836160bbf
Yukito <193806322+yukito-create@users.noreply.github.com>
2025-12-06 16:12:21 +0900

    list completed

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,17 +1,21 @@
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, FlatList } from "react-native"
 import { router, useNavigation } from "expo-router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore"
 
 import MemoListItem from "../../components/MemoListItem"
 import CircleButton from "../../components/CircleButton"
 import Icon from "../../components/Icon"
 import LogOutButton from "../../components/LogOutButton"
+import { db, auth } from "../../config"
+import { type Memo } from "../../../types/memo"
 
 const handlePress = (): void => {
   router.replace("/memo/create")
 }
 
 const List = (): React.JSX.Element  => {
+  const [memos, setMemos] = useState<Memo[]>([])
   const navigation = useNavigation()
 
   useEffect(() => {
@@ -21,13 +25,32 @@ const List = (): React.JSX.Element  => {
     })
   }, [])
 
+  useEffect(() => {
+    if (auth.currentUser === null) { return }
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    const q = query(ref, orderBy("updatedAt", "desc"))
+    const unsubscribe = onSnapshot(q, (snapShot) => {
+      const remoteMemos: Memo[] = []
+      snapShot.forEach((doc) => {
+        console.log("memo", doc.data())
+        const { bodyText, updatedAt } = doc.data()
+        remoteMemos.push({
+          id: doc.id,
+          bodyText,
+          updatedAt
+        })
+      })
+      setMemos(remoteMemos)
+    })
+    return unsubscribe
+  }, [])
+
   return (
     <View style={styles.container}>
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
+      <FlatList
+        data={memos}
+        renderItem={({ item }) => <MemoListItem memo={item} />}
+      />
       <CircleButton onPress={handlePress}>
         <Icon name="plus" size={40} color="#ffffff" />
       </CircleButton>
--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -2,14 +2,24 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
 import { Link } from "expo-router"
 
 import Icon from "./Icon"
+import { type Memo } from "../../types/memo"
 
-const MemoListItem = (): React.JSX.Element => {
+interface Props {
+  memo: Memo
+}
+
+const MemoListItem = (props: Props): React.JSX.Element | null => {
+  const { memo } = props
+  const { bodyText, updatedAt } = memo
+  if (bodyText === null || updatedAt === null) { return null }
+  const dateString = updatedAt.toDate().toLocaleString("ja-JP")
+
   return (
     <Link href="/memo/detail" asChild>
       <TouchableOpacity style={styles.memoListItem}>
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+          <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
+          <Text style={styles.memoListItemDate}>{dateString}</Text>
         </View>
         <TouchableOpacity>
           <Icon name="delete" size={32} color="#B0B0B0" />
